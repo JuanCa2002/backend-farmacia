3fe7832a3129e85b0a6cd85ba130f58f91891853
Juan Camilo Torres Beltran <torres.juan.3055@eam.edu.co>
2023-08-08 14:53:21 -0500

    Creando pruebas unitarias de medicinas y ventas

---
+++ medicine-service/src/test/java/com/medicineservice/medicineservice/controller/MedicineControllerTest.java
@@ -0,0 +1,2 @@
+package com.medicineservice.medicineservice.controller;public class MedicineControllerTest {
+}
---
+++ ventas-service/src/test/java/com/ventasservice/ventasservice/controller/SaleControllerTest.java
@@ -0,0 +1,2 @@
+package com.ventasservice.ventasservice.controller;public class SaleControllerTest {
+}
